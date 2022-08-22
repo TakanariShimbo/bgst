ad6c9c8482befdf5ad1bbb7cf588ff6f9f28619e
TakanariShimbo <takanari.shimbo+github@gmail.com>
2022-08-22 18:35:07 +0900

    revise

--- convert_movie2images.py
+++ convert_movie2images.py
@@ -1,36 +1,49 @@
+import argparse
 import os
 import cv2
 import tqdm
 
 
-# setting
-filename = 'test_1'     # string
-total_frame = 1001      # int
-down_fps_rate = 4       # int
+def main():
+    # read movie
+    video_path = f"../datas/original_movies/{opt.filename}.mp4"
+    cap = cv2.VideoCapture(video_path)
 
-# read movie
-video_path = f"../datas/original_movies/{filename}.mp4"
-cap = cv2.VideoCapture(video_path)
+    # save directory
+    image_folder_path = f"../datas/original_images/{opt.filename}/"
+    os.makedirs(image_folder_path, exist_ok=True)
 
-# save directory
-image_folder_path = f"../datas/original_images/{filename}/"
-os.makedirs(image_folder_path, exist_ok=True)
+    with tqdm.tqdm(total=opt.total_frame) as pbar:
+        cnt = 0
+        while True:
+            # get frame
+            ret, img_bgr = cap.read()
+            if not ret:
+                break
+            if cnt >= opt.total_frame * opt.down_fps_rate:
+                break
 
-with tqdm.tqdm(total=total_frame) as pbar:
-    cnt = 0
-    while True:
-        # get frame
-        ret, img_bgr = cap.read()
-        if not ret:
-            break
-        if cnt >= total_frame*down_fps_rate:
-            break
+            # save
+            if cnt%opt.down_fps_rate == 0:
+                cv2.imwrite(image_folder_path + f"frame{cnt//opt.down_fps_rate}.bmp", img_bgr)
+                pbar.update(1)
+            cnt += 1
 
-        # save
-        if cnt%down_fps_rate == 0:
-            cv2.imwrite(image_folder_path + f"frame{cnt//down_fps_rate}.bmp", img_bgr)
-            pbar.update(1)
-        cnt += 1
+        cap.release()
+        cv2.destroyAllWindows()
 
-    cap.release()
-    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    # get args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', type=str)
+    parser.add_argument('--total_frame', type=int)
+    parser.add_argument('--down_fps_rate', type=int)
+    opt = parser.parse_args()
+    print(opt)
+
+    # run main
+    main()
+
+    # ex)
+    # python convert_movie2images.py --filename "test_1" --total_frame 1001 --down_fps_rate 4
--- make_mask.py
+++ make_mask.py
@@ -1,10 +1,9 @@
+import argparse
+import glob
 import os
 import cv2
-import glob
 import tqdm
 
-# setting
-filename = 'test_1'
 
 # define opening
 # kernel = np.ones((5,5),np.uint8)
@@ -16,37 +15,52 @@ def customized_opening(img, scale, iteration):
     return img
 
 
-# define bgst model
-# Ref: https://pystyle.info/opencv-background-substraction/
-bgst = cv2.createBackgroundSubtractorMOG2(
-    history=15, varThreshold=50, detectShadows=False
-)
+def main():
+    # define bgst model
+    # Ref: https://pystyle.info/opencv-background-substraction/
+    bgst = cv2.createBackgroundSubtractorMOG2(
+        history=15, varThreshold=50, detectShadows=False
+    )
 
-# save directory
-save_folder_path = f"../datas/mask_images/{filename}/"
-os.makedirs(save_folder_path, exist_ok=True)
+    # save directory
+    save_folder_path = f"../datas/mask_images/{opt.filename}/"
+    os.makedirs(save_folder_path, exist_ok=True)
 
-# read directory
-image_folder_path = f"../datas/original_images/{filename}/"
-total_frame = len(glob.glob(image_folder_path+'*.bmp'))
+    # read directory
+    image_folder_path = f"../datas/original_images/{opt.filename}/"
+    total_frame = len(glob.glob(image_folder_path+'*.bmp'))
 
-for i in tqdm.tqdm(range(total_frame)):
-    # read images
-    image_path = image_folder_path + f"frame{i}.bmp"
-    img_bgr = cv2.imread(image_path)
+    for i in tqdm.tqdm(range(total_frame)):
+        # read images
+        image_path = image_folder_path + f"frame{i}.bmp"
+        img_bgr = cv2.imread(image_path)
 
-    img_mask_gray = bgst.apply(img_bgr)
+        img_mask_gray = bgst.apply(img_bgr)
 
-    # opening
-    # opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=3)
-    img_mask_gray = customized_opening(img=img_mask_gray, scale=2, iteration=10)
-    img_mask_gray[img_mask_gray != 0] = 255
+        # opening
+        # opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=3)
+        img_mask_gray = customized_opening(img=img_mask_gray, scale=2, iteration=10)
+        img_mask_gray[img_mask_gray != 0] = 255
 
-    # viz
-    img_viz_bgr = img_bgr.copy()
-    img_viz_bgr[img_mask_gray == 0] = 0
-    cv2.imshow("Frame (Only Forground)", img_mask_gray)
-    cv2.waitKey(1)
+        # viz
+        img_viz_bgr = img_bgr.copy()
+        img_viz_bgr[img_mask_gray == 0] = 0
+        cv2.imshow("Frame (Only Forground)", img_mask_gray)
+        cv2.waitKey(1)
 
-    # save
-    cv2.imwrite(save_folder_path + f"frame{i}.bmp", img_mask_gray)
+        # save
+        cv2.imwrite(save_folder_path + f"frame{i}.bmp", img_mask_gray)
+
+
+if __name__ == '__main__':
+    # get args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', type=str)
+    opt = parser.parse_args()
+    print(opt)
+
+    # run main
+    main()
+
+    # ex)
+    # python make_mask.py --filename "test_1"
